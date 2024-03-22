b15cf0e0d181ffe6821e49abd373b7538cc579eb
lq-c <2404831604@qq.com>
2024-03-22 18:29:15 +0800

    准备下班了

--- components/layout-header.vue
+++ components/layout-header.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div bg="[rgba(246,248,250)]" class="bg-dark-text-color" w-400px border-rd-24px flex flex-items-center
-    flex-justify-between h-40px px-24px border="1px solid #D0D7DE">
+  <div class="layout-header" w-400px border-rd-24px flex flex-items-center
+    flex-justify-between h-40px px-24px>
     <div>太抽象了</div>
     <div class="font-size-22px">zhuyu</div>
   </div>
